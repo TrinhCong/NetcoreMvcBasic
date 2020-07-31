f768bbfe48b4b3981c9485cf8566bbb6762badab
Trinh.V.Cong <cong.trinh@vietgis.com.vn>
2020-07-31 14:39:13 +0700

    push code

--- NetcoreMvcBasic/Startup.cs
+++ NetcoreMvcBasic/Startup.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Autofac;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -10,7 +14,6 @@ using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using NetcoreMvcBasic.Helpers;
 using NetcoreMvcBasic.Business.Models.Config;
-using Newtonsoft.Json.Serialization;
 
 namespace NetcoreMvcBasic
 {
@@ -23,28 +26,29 @@ namespace NetcoreMvcBasic
 
         public IConfiguration Configuration { get; }
 
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.Configure<CookiePolicyOptions>(options =>
             {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
             });
 
-            services.AddMvc()
-                .AddNewtonsoftJson(options =>
-                {
-                    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
-                    options.SerializerSettings.ContractResolver = new DefaultContractResolver();
 
-                }).AddJsonOptions(jsonOptions => {
-                    jsonOptions.JsonSerializerOptions.PropertyNamingPolicy = null;
-                })
-                .SetCompatibilityVersion(CompatibilityVersion.Latest);
+            services.AddMvc().AddJsonOptions(options =>
+            {
+                options.SerializerSettings.ContractResolver = new DefaultContractResolver();
+                options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                options.SerializerSettings.MetadataPropertyHandling = MetadataPropertyHandling.ReadAhead;
+                options.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
+            }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
             services.Configure<ConnectionConfig>(Configuration.GetSection("ConnectionStrings"));
 
             services.AddSession(option =>
             {
+                //Set session timeout value
                 option.IdleTimeout = TimeSpan.FromSeconds(3600);
                 option.Cookie.HttpOnly = true;
             });
@@ -55,6 +59,7 @@ namespace NetcoreMvcBasic
             AutofacRegistrar.Register(builder);
         }
 
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -64,8 +69,10 @@ namespace NetcoreMvcBasic
             else
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
